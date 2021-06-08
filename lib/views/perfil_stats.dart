3e423477f0fd6f45dcb6b1f581832736ffa2b67f
Wellington Brito <10562424+wellington-brito@users.noreply.github.com>
2021-06-08 11:08:41 +0000

    Remover headers

--- lib/views/perfil_stats.dart
+++ lib/views/perfil_stats.dart
@@ -69,10 +69,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   getProfile(){
     print("CLICOU: "+ nickName);
-    const headers = { "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-      "Origin": "https://developer.riotgames.com",
-      "X-Riot-Token": "RGAPI-c22ca3dc-3246-4bfc-aa71-837033ed33dd"};
+    //headers
 
     http.get(Uri.parse(
         'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nickName), headers: headers)
