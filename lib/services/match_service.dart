13e4a585422a704e2cf1925fc1c566252adbb325
Wellington Brito <10562424+wellington-brito@users.noreply.github.com>
2021-07-05 09:56:46 -0300

    Update match_service.dart

--- lib/services/match_service.dart
+++ lib/services/match_service.dart
@@ -8,7 +8,7 @@ class MatchService {
     "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-6638d9ad-b487-40f2-a13b-b16545f81f96"
+    "X-Riot-Token": ""
   };
 
   getMatches(puuidOtherSummoner) {
