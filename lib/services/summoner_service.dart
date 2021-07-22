0f44317ebfbba74c013a63ea08d58f8b5fc4824b
Wellington Brito <10562424+wellington-brito@users.noreply.github.com>
2021-07-21 23:09:01 -0300

    Update summoner_service.dart

--- lib/services/summoner_service.dart
+++ lib/services/summoner_service.dart
@@ -12,7 +12,7 @@ class SummonerService {
     "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-befebfc0-23fc-4918-8806-b42591419c54"
+    "X-Riot-Token": ""
   };
 
   Future<Summoner> getDataSummoner(nickName) {
